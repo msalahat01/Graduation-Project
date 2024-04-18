a65b27c95dc496d563e116c5348bc11335492ef8
msalahat01 <mohammadsalahat840@gmail.com>
2024-04-18 18:35:27 +0300

    Las

--- src/pages/Joinus.js
+++ src/pages/Joinus.js
@@ -42,7 +42,7 @@ const box1={
   width: "540px",
   borderRadius: "50px",
   left: "17%" ,
-  top: "20%" 
+  top: "17%" 
 }
 
 const box2={
@@ -51,7 +51,7 @@ const box2={
   width: "540px",
   borderRadius: "50px",
   right: "17%" ,
-  top: "20%" 
+  top: "17%" 
 }
 
   return (
@@ -63,16 +63,16 @@ const box2={
                 <NavBar style={{position: 'absolute' , top: "0%"}}></NavBar>
               </CRow>
 
-          <h1 className="tex1111">JOIN US <br></br> AS</h1>
+          <h1 className="tex1111">JOIN US </h1>
             <CRow>
               <CCol sm="auto">
                   <div style = {{...box1 , position: 'absolute' }}></div>
-                  <h1 style = {{position: 'absolute' ,left: "22.5%", color: "#FFFFFF" , top: "22%" }}>GYM ORGANIZAION</h1>
-                  <h2 style = {{position: 'absolute',   color: "#FFFFFF" ,fontsize: "100%", width: "12%" , textalign: 'center' ,lineheight:" 125%" , top: "38%", left:"27%"}}
+                  <h1 style = {{position: 'absolute' ,left: "22.5%", color: "#FFFFFF" , top: "19%" }}>GYM ORGANIZAION</h1>
+                  <h2 style = {{position: 'absolute',   color: "#FFFFFF" ,fontsize: "100%", width: "12%" , textalign: 'center' ,lineheight:" 125%" , top: "35%", left:"27%"}}
                   > Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
 
                   <Button style = {{position: 'absolute', height:"1.3cm" , width: "19%" ,color:"#252422", borderRadius:"22px"
-                   ,backgroundColor: "#EB5E28" ,boxShadow: "3px 3px 2px rgba(46, 46, 46, 0.62)",left: "23%" , top: "64%"}} size="lg" href="/Joinus1"
+                   ,backgroundColor: "#EB5E28" ,boxShadow: "3px 3px 2px rgba(46, 46, 46, 0.62)",left: "23%" , top: "61%"}} size="lg" href="/Joinus1"
                   onMouseEnter={handleMouseEvent}
                   onMouseLeave={handleMouseEvent1} 
                   >Join us</Button>  
@@ -80,15 +80,15 @@ const box2={
 
               <CCol sm="auto"> 
                   <h1 style = {{...box2 , position: 'absolute'}}></h1>
-                  <h1 style = {{position: 'absolute' ,right: "20.5%", color: "#FFFFFF" , top: "22%" }}>COACH OR NUTRITION</h1>
-                  <h2  style = {{position: 'absolute',   color: "#FFFFFF" ,fontsize: "100%", width: "12%" , textalign: 'center' ,lineheight:" 125%" , top: "38%", right:"27%"}}
+                  <h1 style = {{position: 'absolute' ,right: "20.5%", color: "#FFFFFF" , top: "19%" }}>COACH OR NUTRITION</h1>
+                  <h2  style = {{position: 'absolute',   color: "#FFFFFF" ,fontsize: "100%", width: "12%" , textalign: 'center' ,lineheight:" 125%" , top: "35%", right:"27%"}}
                   >Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h2>
 
                   <Button size="lg" href="/Joinus2" 
                   onMouseEnter={handleMouseEvent}
                   onMouseLeave={handleMouseEvent1} 
                   style={{position: 'absolute', height:"1.3cm" , width: "19%" ,color:"#252422", borderRadius:"22px" ,
-                  backgroundColor: "#EB5E28" ,boxShadow: "3px 3px 2px rgba(46, 46, 46, 0.62)" ,right: "23%" , top: "64%"}}
+                  backgroundColor: "#EB5E28" ,boxShadow: "3px 3px 2px rgba(46, 46, 46, 0.62)" ,right: "23%" , top: "61%"}}
                   >Join us</Button> 
               </CCol>
             </CRow>
